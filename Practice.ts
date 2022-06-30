07473ba81cab1d83bbaacca9c3186a2ae7dadf75
YeonHyeEun <21900442@handong.edu>
2022-07-01 03:26:32 +0900

    이전 내용 추가

--- Practice.ts
+++ Practice.ts
@@ -1,3 +1,32 @@
+//이전 공부 내용 
+//   const kimchi = {
+//     맛있어 : true
+// }
+
+// let a = 3; 
+// let b = "hio"; 
+
+// //call signiture 작성 
+// type SuperPrint = {
+//  <TypePlaceholder>(arr: TypePlaceholder[]):TypePlaceholder
+// //(arr: boolean[]):void
+// //(arr: string[]):void
+// //(arr:(number|boolean)[]):void
+// //이런 문제들을 모두 해소해주는 제네릭 타입! 
+// }
+
+// const superPrint: SuperPrint = (arr) => arr[0]
+// //  {
+// //    arr.forEach(i => console.log(i))
+// //  }
+
+// const a = superPrint([1,2,3,4])
+// const b = superPrint([true, false, true])
+// superPrint(["1","2","3"])
+// superPrint([1,2,true, false])
+
+
+
 //제네릭 타입 공부 
 //다형성과 함께 
 
