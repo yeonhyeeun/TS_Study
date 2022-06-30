40b6611834061aecbfb35ddbe8b33094438956d8
YeonHyeEun <21900442@handong.edu>
2022-07-01 04:35:25 +0900

    2번 미해결

--- day4answer.ts
+++ day4answer.ts
@@ -7,9 +7,17 @@ console.log(last([1,2,3,4]))
 // console.log(last(["1","2","hi"]))
 
 
-//2번 배열의 시작에 아이템을 넣고 리턴하는 함수 prepend(arr, item)
-function Print<T>(a: T[]){
-    return a[a.length-1]
-}
+//2번 : 배열의 시작에 아이템을 넣고 리턴하는 함수 prepend(arr, item)
 
-Print([1,2,3,4])
+// function prepend<T>(a: T[], b:T){
+//     b = a[0] 
+//     return b
+// }
+
+type Prepend = {
+    <T>(arr: T[]):T}
+
+const prepend: Prepend = (arr) => arr[0]
+
+const p = prepend([1,2,3,4])
+console.log(p)
