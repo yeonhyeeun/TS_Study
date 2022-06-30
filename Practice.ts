592b774ec16ef7fe80bedec98642dcec6314fe6c
YeonHyeEun <21900442@handong.edu>
2022-07-01 03:24:11 +0900

    add 주석

--- Practice.ts
+++ Practice.ts
@@ -1,12 +1,15 @@
 //제네릭 타입 공부 
 //다형성과 함께 
 
+//마치 벡터와 같은 
 type SuperPrint = {
     <TypePlaceholder>(arr: TypePlaceholder[]): TypePlaceholder
 }
+//<T>로 주로 줄여씀 -> type의 약자 
 
 const superPrint: SuperPrint = (arr) => arr[0] 
 
 const a = superPrint([1,2,3,4])
 const b = superPrint([true, false, false])
-const c = 
+const c = superPrint(["a", "b", "c"])
+const d = superPrint(["a", "b", 1, 2, true])
